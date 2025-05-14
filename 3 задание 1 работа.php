bbc47475fc341c9043276e349e3dd20ee8547913
andi <fqhfn0@gmail.com>
2025-05-14 16:36:58 +0300

    first commit

---
+++ 3 задание 1 работа.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+function calculateAverage(array $grades): float {
+    return array_sum($grades) / count($grades);
+}
+function hasGrades(array $grades): bool {
+    return count($grades) > 0;
+}
+
+$grades = [85, 90, 78, 88, 92, 76, 81, 95, 89, 84];
+
+if (hasGrades($grades)) {
+    $average = calculateAverage($grades);
+    echo "Средняя оценка студентов: " . number_format($average, 2);
+} else {
+    echo "Нет данных для вычисления средней оценки.";
+}
---
+++ 3 задание 2 работа.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+function thirdMax(array $nums): int {
+    $uniqueNums = array_unique($nums); 
+    rsort($uniqueNums); 
+
+    if (count($uniqueNums) < 3) {
+        return max($uniqueNums);
+    } else {
+        return $uniqueNums[2];
+    }
+}
+
+$numbers = [5, 1, 9, 4, 7, 2, 9, 10, 3, 6, 8, 10];
+
+echo "Третье максимальное число: " . thirdMax($numbers);

cbd5cb08317b78353515c4650099e7ab534a018a
ANDiGRA <59337706+ANDiGRA@users.noreply.github.com>
2025-12-19 20:36:21 +0300

    Update 3 задание 3 работа.php

--- 3 задание 3 работа.php
+++ 3 задание 3 работа.php
@@ -1,21 +1,25 @@
 <?php
+
 declare(strict_types=1);
-function calculateAverage(array $grades): float {
+function calculateAverage(array $grades): float
+{
     return array_sum($grades) / count($grades);
 }
-function isExcellentStudent(array $student): bool {
+function isExcellentStudent(array $student): bool
+{
     return calculateAverage($student['grades']) >= 4.0;
 }
 
-function printStudentAverage(array $student): void {
+function printStudentAverage(array $student): void
+{
     $average = calculateAverage($student['grades']);
     echo $student['name'] . ": " . number_format($average, 2) . "\n";
 }
 
-$students = [ 
+$students = [
     ['name' => 'Никита', 'age' => 17, 'grades' => [5, 4, 5]],
     ['name' => 'Дима', 'age' => 17, 'grades' => [3, 4, 2]],
-    ['name' => 'Данил', 'age' => 17, 'grades' => [4, 5, 5]], 
+    ['name' => 'Данил', 'age' => 17, 'grades' => [4, 5, 5]],
 ];
 
 foreach ($students as $student) {
